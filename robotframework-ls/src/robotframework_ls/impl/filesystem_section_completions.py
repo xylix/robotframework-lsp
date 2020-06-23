60435b110a795c4a8471a219c834021c501191b5
Fabio Zadrozny <fabiofz@gmail.com>
2020-06-23 10:48:18 -0300

    wip

--- robotframework-ls/src/robotframework_ls/impl/filesystem_section_completions.py
+++ robotframework-ls/src/robotframework_ls/impl/filesystem_section_completions.py
@@ -37,31 +37,52 @@ def _create_completion_item(library_name, selection, token, start_col_offset=Non
     ).to_dict()
 
 
-def add_completions_from_dir(directory, matcher, ret, sel, token, qualifier):
+def add_completions_from_dir(
+    completion_context,
+    directory,
+    matcher,
+    ret,
+    sel,
+    token,
+    qualifier,
+    extensions,
+    skip_current,
+):
+    from robocode_ls_core import uris
+
+    def normfile(path):
+        return os.path.normpath(os.path.normcase(path))
+
+    curr_file = normfile(uris.to_fs_path(completion_context.doc.uri))
+
     for filename in os.listdir(directory):
-        check = None
-        if filename.endswith(".py"):
-            check = filename
+        use_path = None
+        if filename.endswith(extensions):
+            # If that'd be a match for the current .robot file, don't show it.
+            if skip_current and curr_file == normfile(
+                os.path.join(directory, filename)
+            ):
+                continue
+
+            use_path = filename
 
         elif filename not in ("__pycache__", ".git") and os.path.isdir(
             os.path.join(directory, filename)
         ):
-            check = filename + "/"
+            use_path = filename + "/"
+        else:
+            continue
 
-        if check is not None:
-            if matcher.accepts(check):
-                ret.append(
-                    _create_completion_item(
-                        check, sel, token, start_col_offset=sel.col - len(qualifier)
-                    )
+        if matcher.accepts(use_path):
+
+            ret.append(
+                _create_completion_item(
+                    use_path, sel, token, start_col_offset=sel.col - len(qualifier)
                 )
+            )
 
 
-def _get_resource_completions(completion_context, token):
-    return []
-
-
-def _get_library_completions(completion_context, token):
+def _get_completions(completion_context, token, match_libs, extensions, skip_current):
     from robotframework_ls.impl.string_matcher import RobotStringMatcher
     from robocode_ls_core import uris
     from robotframework_ls.impl.robot_constants import BUILTIN_LIB
@@ -77,23 +98,27 @@ def _get_library_completions(completion_context, token):
 
     if os.path.isabs(token.value):
         add_completions_from_dir(
+            completion_context,
             value_to_cursor_split[0],
             RobotStringMatcher(value_to_cursor_split[1]),
             ret,
             sel,
             token,
             value_to_cursor_split[1],
+            extensions,
+            skip_current=skip_current,
         )
 
     else:
-        matcher = RobotStringMatcher(value_to_cursor)
-        libspec_manager = completion_context.workspace.libspec_manager
-        library_names = set(libspec_manager.get_library_names())
-        library_names.discard(BUILTIN_LIB)
+        if match_libs:
+            matcher = RobotStringMatcher(value_to_cursor)
+            libspec_manager = completion_context.workspace.libspec_manager
+            library_names = set(libspec_manager.get_library_names())
+            library_names.discard(BUILTIN_LIB)
 
-        for library_name in library_names:
-            if matcher.accepts(library_name):
-                ret.append(_create_completion_item(library_name, sel, token))
+            for library_name in library_names:
+                if matcher.accepts(library_name):
+                    ret.append(_create_completion_item(library_name, sel, token))
 
         # After checking the existing library names in memory (because we
         # loaded them at least once), check libraries in the filesystem.
@@ -104,9 +129,33 @@ def _get_library_completions(completion_context, token):
         matcher = RobotStringMatcher(value_to_cursor_split[1])
         directory = os.path.join(dirname, value_to_cursor_split[0])
         add_completions_from_dir(
-            directory, matcher, ret, sel, token, value_to_cursor_split[1]
+            completion_context,
+            directory,
+            matcher,
+            ret,
+            sel,
+            token,
+            value_to_cursor_split[1],
+            extensions,
+            skip_current=skip_current,
         )
     return ret
+
+
+def _get_resource_completions(completion_context, token):
+    return _get_completions(
+        completion_context,
+        token,
+        False,
+        (".resource", ".robot", ".txt"),
+        skip_current=True,
+    )
+
+
+def _get_library_completions(completion_context, token):
+    return _get_completions(
+        completion_context, token, True, (".py",), skip_current=False
+    )
 
 
 def complete(completion_context):
